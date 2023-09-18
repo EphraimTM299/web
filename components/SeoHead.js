1e8fca2da15d0ee2c4572f10d4e932ef38c79f9b
Ephraimtm299 <ephraimtamuka@gmail.com>
2023-09-18 13:25:49 +0200

    Update

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -14,7 +14,7 @@ const defaultMeta = {
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
+  // image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
   author: 'Ephraim Makurumidze'
 };
 
@@ -67,7 +67,7 @@ const SeoHead = (props) => {
       <meta name='twitter:title' content={meta.title} />
       <meta name='twitter:description' content={meta.description} />
       <meta name='twitter:image' content={meta.image} />
-      {meta.date && (
+      {/* {meta.date && (
         <>
           <meta property='article:published_time' content={meta.date} />
           <meta
@@ -81,7 +81,7 @@ const SeoHead = (props) => {
             content={meta.author}
           />
         </>
-      )}
+      )} */}
       {/* Favicons */}
       {favicons.map((linkProps) => (
         <link key={linkProps.href} {...linkProps} />
@@ -90,7 +90,7 @@ const SeoHead = (props) => {
       <meta name='msapplication-TileColor' content='#3b82f6' />
       <meta
         name='msapplication-TileImage'
-        content='/favicon/ms-icon-144x144.png'
+        content='/favicon/android-chrome-192x192.png'
       />
       {/* Accent color on supported browser */}
       <meta name='theme-color' content='#3b82f6' />
@@ -103,37 +103,37 @@ const favicons = [
   {
     rel: 'apple-touch-icon',
     sizes: '57x57',
-    href: '/favicon/apple-icon-57x57.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'apple-touch-icon',
     sizes: '60x60',
-    href: '/favicon/apple-icon-60x60.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'apple-touch-icon',
     sizes: '72x72',
-    href: '/favicon/apple-icon-72x72.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'apple-touch-icon',
     sizes: '76x76',
-    href: '/favicon/apple-icon-76x76.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'apple-touch-icon',
     sizes: '114x114',
-    href: '/favicon/apple-icon-114x114.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'apple-touch-icon',
     sizes: '120x120',
-    href: '/favicon/apple-icon-120x120.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'apple-touch-icon',
     sizes: '144x144',
-    href: '/favicon/apple-icon-144x144.png',
+    href: '/favicon/apple-touch-icon.png',
   },
   {
     rel: 'apple-touch-icon',
@@ -147,7 +147,7 @@ const favicons = [
   },
   {
     rel: 'mask-icon',
-    href: '/favicon/safari-pinned-tab.svg',
+    href: '/favicon/apple-touch-icon.png',
     color: '#F59A9A',
   },
   {
@@ -169,14 +169,8 @@ const favicons = [
   {
     rel: 'icon',
     type: 'image/png',
-    sizes: '96x96',
-    href: '/favicon/favicon-96x96.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
     sizes: '192x192',
-    href: '/favicon/android-icon-192x192.png',
+    href: '/favicon/android-chrome-192x192.png',
   },
   {
     rel: 'manifest',
